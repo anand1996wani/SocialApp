37af5d1a0b4990823c94811865bc10decc14ec37
Anand Wani <anand.wani1996@gmail.com>
2020-04-01 19:30:38 +0530

    Likes and UnLike Route

--- socialapp-functions/functions/handlers/screams.js
+++ socialapp-functions/functions/handlers/screams.js
@@ -109,5 +109,84 @@ exports.commentOnScream = (request, response) => {
 
 // Like a scream
 exports.likeScream = (request, response) => {
-    
-}
+    const likeDocument = db.collection('likes').where('userHandle', '==', request.user.handle)
+        .where('screamId', '==', request.params.screamId).limit(1);
+
+    const screamDocument = db.doc(`/screams/${request.params.screamId}`);
+
+    let screamData = {};
+
+    screamDocument.get()
+        .then(doc => {
+            if(doc.exists){
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get();
+            } else {
+                return response.status(404).json({ error: 'Scream not found'});
+            }
+        })
+        .then(data => {
+            if(data.empty){
+                return db.collection('likes').add({
+                    screamId: request.params.screamId,
+                    userHandle: request.user.handle
+                })
+                .then(() => {
+                    screamData.likeCount++;
+                    return screamDocument.update({ likeCount: screamData.likeCount });
+                })
+                .then(() => {
+                    return response.json(screamData);
+                })
+            } else {
+                return response.status(400).json({ error: 'Scream already liked'});
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            response.status(500).json({ error: err.code });
+        });
+};
+
+exports.unlikeScream = (request, response) => {
+    const likeDocument = db
+        .collection('likes')
+        .where('userHandle', '==', request.user.handle)
+        .where('screamId', '==', request.params.screamId)
+        .limit(1);
+
+
+    const screamDocument = db.doc(`/screams/${request.params.screamId}`);
+    let screamData = {};
+
+    screamDocument
+        .get()
+        .then((doc) => {
+            if(doc.exists) {
+                screamData = doc.data();
+                screamData.screamId = doc.id;
+                return likeDocument.get();
+            } else{
+                return response.status(404).json({ error: 'Scream not found' });
+            }
+        })
+        .then((data) => {
+            if(data.empty) {
+                return response.status(400).json({ error: 'Scream not liked' });
+            }else {
+                return db.doc(`/likes/${data.docs[0].id}`).delete()
+                    .then(() => {
+                        screamData.likeCount--;
+                        return screamDocument.update({ likeCount: screamData.likeCount });
+                    })
+                    .then(() => {
+                        response.json(screamData);
+                    })
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            response.status(500).json({ error: err.code });
+        });
+};
